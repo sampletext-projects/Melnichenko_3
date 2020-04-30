feeea97de6e923b3b93b8e80a0547fdea720f22a
sampletext32 <sampletext32@bk.ru>
2020-04-30 19:47:09 +0300

    Add project

---
+++ Melnichenko_3/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	cout << "Выполнила Мельниченко Марина (УТН-111)" << endl;
+	cout << "Программа для нахождения степеней числа" << endl;
+
+	int k, n, t, i;
+	cout << "Введите K: ";
+	cin >> k;
+	cout << "Введите N: ";
+	cin >> n;
+	t = k;
+	i = 1;
+	while(i <= n)
+	{
+		cout << k << "^" << i << " = " << t << endl;
+		t *= k;
+		i++;
+	}
+
+	system("pause");
+	return 0;
+}
